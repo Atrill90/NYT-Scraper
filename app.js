57185f7b63d5f9c9dfa4b7a491c67e704f636053
Alex Trilling <atrill90@gmail.com>
2018-05-21 19:34:17 -0400

    Heroku stuff

--- app.js
+++ app.js
@@ -34,9 +34,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/nytscraper")
-.then(()=>{
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytscraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI).then(()=>{
     console.log("mongo db connected");
 });
 
